a5fdfb39cac6650305576361d9d21ef52897a9a5
elizhadjinian <70875468+elizhadjinian@users.noreply.github.com>
2020-11-26 17:49:46 -0800

    added link to github

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+      <small>Coded by <a href = "https://github.com/elizhadjinian/weather-app-react" target="blank">Elizabeth Hadjinian</a></small>
     </div>
   );
 }
