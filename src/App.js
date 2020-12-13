25e9364bf4fc22937d7c2126b85118c00b133a16
elizhadjinian <70875468+elizhadjinian@users.noreply.github.com>
2020-12-12 22:45:31 -0800

    added bootstrap and border

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Weather from "./Weather.js";
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
 export default function App() {
   return (
