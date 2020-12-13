e1738314614903334564d529f1530e96e3481155
elizhadjinian <70875468+elizhadjinian@users.noreply.github.com>
2020-12-12 22:39:12 -0800

    removed title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Weather from "./Weather.js";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
       <Weather />
       <small>Coded by <a href = "https://github.com/elizhadjinian/weather-app-react" target="blank">Elizabeth Hadjinian</a></small>
     </div>
