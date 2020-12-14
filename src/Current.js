7a67712cc2c537571e5b55c8721168a59b5e1efc
elizhadjinian <70875468+elizhadjinian@users.noreply.github.com>
2020-12-13 18:27:48 -0800

    added header line

--- src/Current.js
+++ src/Current.js
@@ -1,5 +1,9 @@
 import React from "react"
+import "./Current.css"
 
 export default function Current (){
-  return <p>test</p>
+  return <div className = "Header" >
+    <h1>New York, NY  </h1> 
+    <h2>15°C | °F</h2>
+    </div>
 }
